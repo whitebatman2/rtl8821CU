bb1ede6ef2eaee06128ab68360a4a0b5d62ce18f
Julien Kohler <kohlerj@users.noreply.github.com>
2018-10-25 17:11:48 +0200

    Added DWA171 hardware revision C1

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -251,6 +251,7 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDER_ID_REALTEK, 0xC811, 0xff, 0xff, 0xff), .driver_info = RTL8821C}, /* 8811CU */
 	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDER_ID_REALTEK, 0x8811, 0xff, 0xff, 0xff), .driver_info = RTL8821C}, /* 8811CU */	
 	/*=== Customer ID ===*/
+	{USB_DEVICE(0x2001, 0x331d),.driver_info = RTL8821C}, /* D-Link - Cameo */
 #endif
 
 	{}	/* Terminating entry */
