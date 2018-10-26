ba0153c6cc628685b0080a484c1fe12e3c428cbc
Julien Kohler <kohler.julien@gmail.com>
2018-10-26 11:37:36 +0200

    updates

--- automountDWA171.sh
+++ automountDWA171.sh
@@ -2,27 +2,39 @@
 
 # Sudo check
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as elevated user"
+  then echo "Please run this script with sudo"
   exit
 fi
 
 # Add script to switch usb mode
 echo 'Adding script to switch USB mode to /usr/bin'
-touch /usr/bin/dwa171script
-echo '#!/bin/bash' > /usr/bin/dwa171script
-echo 'usb_modeswitch -KW -v 0bda -p 1a2b' >> /usr/bin/dwa171script
+
+SCRIPT=/usr/bin/dwa171script
+cat > $SCRIPT << EOL
+#!/bin/bash
+usb_modeswitch -KW -v 0bda -p 1a2b
+EOL
 
 # Make it executable
 echo 'Making it executable'
-chmod +x /usr/bin/dwa171script
+   
+if [ -e $SCRIPT ]; then
+   chmod +x $SCRIPT
+else
+   echo "File $SCRIPT does not exist."   
+fi
 
 # Create rule to run script as dongle gets mounted
 echo 'Adding udev rule'
-echo 'ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="1a2b", RUN+="/usr/bin/dwa171script"' | tee /etc/udev/rules.d/dwa171.rules
+
+cat > /etc/udev/rules.d/dwa171.rules << EOL
+ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="1a2b", RUN+="/usr/bin/dwa171script"
+EOL
 
 # Reload rules
 echo 'Reloading udev rules'
+
 udevadm control --reload-rules
 
 # Done
-echo 'Done'
+echo 'Done, try plugging in the dongle'
